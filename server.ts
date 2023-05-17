c26b5ad81ac6ac7397e42e1d1fa6753842656d20
Gabriel Aranda <per.gabrielaranda@gmail.com>
2023-05-17 11:10:07 -0300

    moved redirect to .env

--- server.ts
+++ server.ts
@@ -13,8 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/login', async function (req, res, next) {
   const authCodeUrlParameters = {
     scopes: ["openid", "profile", "email"],
-    //  redirectUri: "http://localhost:3000/redirect",
-    redirectUri: "https://dashboard.hubees.com.br/login",
+    redirectUri: process.env.REDIRECT_TO!,
   };
 
   const authUrl = await client.getAuthCodeUrl(authCodeUrlParameters);
@@ -24,11 +23,12 @@ app.get('/login', async function (req, res, next) {
 
 app.get('/getToken', async (req, res) => {
   const response = await client.acquireTokenByCode({
-    code: '0.ASUAlyLYIerkJEeps7L7xGdbpHDJtnOKIg5AulU19PiB_KklALQ.AgABAAIAAAD--DLA3VO7QrddgJg7WevrAgDs_wUA9P8M8ZlJf4-ag_agRUZvt5SS1oAiqkq2fBY-e_x7rDganJvh71ZwuKYWkBumZOh5qg4g6JvfvdwjNjzbijsZ5CS9pmxlg7WEC7dQNTuJctGlf1W0pZsuOT0FHTeHcyAhJktEecPnt4TI5eKVw6OBZlC_7zjjROgcT20arRF5aLz9OV5zW1xzzWxzFI7s2c5JzmnCVnLZmgKZidx5DJ6hLbgxxIFjk7gbcMIYH8KVgbQy7EtT6lsXosuNZgQoeGru8_HtlNgptem_LqyXBz3x8oDyI2VSW4zrvhQHy-Gzd0RCHTsvsQP70lbedPpCZgXnAuNuFJwa3D_EmfCZaDD7KZ5pleEJmiDBbiu8eiPn881DL4h_9Ulz5c5C2yXtIK0vcq4lj1CFJ66GC5UUvSVlFlVqDe7XvvHWFUPNpx3eQOtxKuEvFeOXOiiFotHDYiW5jMxHfkX7__94Rm1sV8IUIkrmaSy0vu8txCp0fnA8b9C31rCEiYnpqQH8BPNUqC7Zi09H6LRE76V-dnTn-juuSkre2yVorhOvJrk_Z7z6FbRhQX3j6w5lXySvI25inIc4PXmKb9KiH1cQJm-z8mg-utUGAUZEZ-njpeTrvqYI0BEL_3JF27dTw1vFiMFsYozaS4qGLOPDWbYP6T6sPF30fsjcEhIOcjhZiKo04BKMv0XBCTDDlTBVGlRp7WYlIGUukvxTOznYKK0SXmUABJmJWFtR2Q',
-    redirectUri: 'https://dashboard.hubees.com.br/login',
+    code: '0.ASUAlyLYIerkJEeps7L7xGdbpHDJtnOKIg5AulU19PiB_KklALQ.AgABAAIAAAD--DLA3VO7QrddgJg7WevrAgDs_wUA9P-ORpn093aUtXtAX4UvB4ButRXqY6njwHyt1n1_7Bejl2yy8AY2o3CKYav7xq3kNwXrY-azEbBu67QobhzCyxCVK8QkMj-iZ2JqZn2OKVMxeWjx2dhjpQK1K4s9PBATUNEj6VvmlJqMNN6wCtOh-0a4_YkMam0IIZwNRgiyt38vMwk1sCvLuCQqA1OZynirhYO37byedl17bXMJ_v5WGps-cijB2k2Xh598KCyL04_QpUCTOK0IPAhaT_yCxPS_tZeawufiLjARFQjhK7xLB_-fc1CV95T0g2ZrWrysAUYsKQehY4odk8L-WywiE5kdLqnu0dpBCvpI-n3FNmntWfmYh1piEr6JrqU93oXnYdNS1xlawM4uIa4BvOTmnylOV2kKzhBzcys0KwB6q6RWGy6j5ZXbbxig-ey1RSPkjwTOL1gHkMZ6ruBkThkSkHlcyNlRIFo6bNiSQVeNucsUTywG3dh4_XCQJ9tpDhrHvI2qMnTIdzzdEaAvcgpkHrZdT02kEIRHWcfj0ZrIQjsqjoatM3S8BgRJ-_cEhbsa2RfbKZ1O1OXBMar4SMIPR1d2i2nbIlSkXBW4OXp6BKI21EuH63FwCg3fUXQ9RTsm5e-z16P8_VIFJXgesxLKG4dDlBB37WEdc2OAzsWrC4uRv52_6jsPIfCDKUiKjyWACNI6mp3uk3Vu2DIVH6Mr9Ztb91ggG7YUaPy3J0ny8dVDSYKbvA',
+    redirectUri: process.env.REDIRECT_TO!,
     scopes: ['email', 'profile']
   })
   console.log(response)
+  return res.status(200).json(response)
 });
 
 app.get('/redirect', (req, res) => {
